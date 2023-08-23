b285e3d812954a1c900b37332947c95372bd2946
eriosta <eri@sienaspace.com>
2023-08-22 19:13:29 -0500

    Report

--- app.py
+++ app.py
@@ -1,4 +1,24 @@
 import streamlit as st
+from docx import Document
+from io import BytesIO
+
+def generate_report(T_value, X, Y, percentage, fsm, m_values):
+    # Create a new document
+    doc = Document()
+
+    # Populate the report content
+    doc.add_heading('PROCEDURE', level=1)
+    doc.add_paragraph(f"{st.text_input('Input Procedure:', 'GRE/SE EPI')} MR elastography and chemical shift-encoded GRE sequences were performed for liver fibrosis, fat, and iron quantification on a {T_value} Tesla scanner.")
+    
+    doc.add_heading('MR Liver Elastography', level=1)
+    doc.add_paragraph(f"Mean liver stiffness (weighted mean of {len(m_values)} measurements): {fsm:.2f} kPa (range {min(m_values):.2f} – {max(m_values):.2f} kPa).")
+    doc.add_paragraph("Interpretation of MR elastography results. Mean LSM:")
+    # ... add the rest of the content similarly ...
+    
+    # Create an in-memory binary stream to save doc to
+    stream = BytesIO()
+    doc.save(stream)
+    return stream
 
 def calculate_iron(T_value, X):
     if T_value == '1.5 T':
@@ -47,34 +67,49 @@ def fsm_grading(fsm):
 
 st.title('MR Elastography App')
 
-# Sidebar for navigation
-options = ['FSM Calculation', 'Steatosis Grade', 'LIC Calculation', 'Clear Results']
-choice = st.sidebar.selectbox('Choose an option:', options)
+# Calculate FSM
+st.header('Calculate FSM')
+m_values = [st.number_input(f'Enter ROI{i} LSM:', value=0.0) for i in range(1, 5)]
+a_values = [st.number_input(f'Enter ROI{i} area:', value=0.0) for i in range(1, 5)]
 
-# Depending on the user's choice, display the corresponding section
-if choice == 'FSM Calculation':
-    st.header('Calculate FSM')
-    m_values = [st.number_input(f'Enter ROI{i} LSM:', value=0.0) for i in range(1, 5)]
-    a_values = [st.number_input(f'Enter ROI{i} area:', value=0.0) for i in range(1, 5)]
-    fsm = calculate_fsm(m_values, a_values)
-    st.write(f"FSM: {fsm} kPa")
-    st.write(fsm_grading(fsm))
+if st.button('Calculate FSM'):
+    if sum(a_values) == 0:
+        st.error("Sum of ROI areas cannot be zero!")
+    else:
+        fsm = calculate_fsm(m_values, a_values)
+        st.write(f"FSM: {fsm} kPa")
+        st.write(fsm_grading(fsm))
 
-elif choice == 'Steatosis Grade':
-    st.header('Determine Steatosis Grade')
-    percentage = st.slider('Enter PDFF:', min_value=0.0, max_value=100.0, value=0.0)
+# Determine Steatosis Grade
+st.header('Determine Steatosis Grade')
+percentage = st.slider('Enter PDFF:', min_value=0.0, max_value=100.0, value=0.0)
+if st.button('Calculate Steatosis Grade'):
     st.write(fat_grading(percentage))
 
-elif choice == 'LIC Calculation':
-    st.header('Calculate LIC')
-    T_values = ['1.5 T', '2.89 T', '3.0 T']
-    T_value = st.selectbox('Select MR:', T_values)
-    X = st.number_input('Enter R2* Value:', value=0.0)
+# Calculate LIC
+st.header('Calculate LIC')
+T_values = ['1.5 T', '2.89 T', '3.0 T']
+T_value = st.selectbox('Select MR:', T_values)
+X = st.number_input('Enter R2* Value:', value=0.0)
+
+if st.button('Calculate LIC'):
     Y = calculate_iron(T_value, X)
     st.write(f"LIC: {Y} mg/g")
     st.write(iron_grading(Y))
 
-elif choice == 'Clear Results':
-    st.header('Clear Results')
-    if st.button('Clear'):
-        st.experimental_rerun()
+# Clear results
+st.header('Clear Results')
+if st.button('Clear'):
+    st.experimental_rerun()
+
+# Section to generate report
+st.header('Generate Report')
+if st.button('Generate Report'):
+    stream = generate_report(T_value, X, Y, percentage, fsm, m_values)
+    buf = BytesIO(stream.getvalue())
+    st.download_button(
+        label="Download Report",
+        data=buf,
+        file_name='MRElastographyReport.docx',
+        mime='application/vnd.openxmlformats-officedocument.wordprocessingml.document'
+    )
