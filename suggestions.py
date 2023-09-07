e213a96643cb0a2194540bdfe2126c330f38f23e
Eri <eri@sienaspace.com>
2023-09-06 21:33:50 -0500

    no math

--- suggestions.py
+++ suggestions.py
@@ -5,10 +5,6 @@ import gspread
 from oauth2client.service_account import ServiceAccountCredentials
 import re
 
-# Initialize session state
-st.session_state['num1'] = st.session_state.get('num1', random.randint(1, 10))
-st.session_state['num2'] = st.session_state.get('num2', random.randint(1, 10))
-
 # Load secret JSON key for Google Sheets authentication
 json_key = {
     "type": st.secrets["my_secrets"]["type"],
@@ -35,10 +31,6 @@ def setup_gspread():
 def append_to_sheet(worksheet, name, email, message, category):
     worksheet.append_row([name, email, message, category])
 
-def generate_random_string(length=5):
-    letters = string.digits  # Use all lowercase letters and digits
-    return ''.join(random.choice(letters) for i in range(length))
-
 def user_suggestions():
     with st.form(key='my_form'):
         st.write("Please fill out this form:")
@@ -46,24 +38,16 @@ def user_suggestions():
         email = st.text_input(label='Enter your email', value='', key='email')
         message = st.text_area(label='Enter your message', value='', key='message')
         category = st.selectbox('Select a category', ['','Error', 'New Request', 'Suggestion', 'Inquiry', 'Other'])
-        st.write(f"Please solve this simple math problem to verify you're not a bot: {st.session_state['num1']} + {st.session_state['num2']}")
-        user_answer = st.number_input(label='Your answer to the math problem:', step=1, format="%d")
+
         submit_button = st.form_submit_button(label='Submit')
         if submit_button:
-            if name and email and message and category and user_answer:
+            if name and email and message and category:
                 # Validate email
                 if re.match(r"[^@]+@[^@]+\.[^@]+", email):
                     correct_answer = st.session_state['num1'] + st.session_state['num2']
-                    if int(user_answer) == correct_answer:
-                        worksheet = setup_gspread()
-                        append_to_sheet(worksheet, name, email, message, category)
-                        st.success("Your message has been sent!")
-                        # Reset the numbers so that new ones will be generated for the next time
-                        st.session_state['num1'] = random.randint(1, 10)
-                        st.session_state['num2'] = random.randint(1, 10)
-                    else:
-                        st.error(f"Incorrect! The correct answer is {correct_answer}. Please verify you are a human!")
+
                 else:
                     st.error("Invalid email address. Please enter a valid email.")
             else:
                 st.error("All fields are required. Please fill out all fields.")
+
